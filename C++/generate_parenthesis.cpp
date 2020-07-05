971fb62983965c90084ae4181751b473ae76496e
Christian Guardiola <61804256+danchris1029@users.noreply.github.com>
2020-07-05 12:35:08 -0500

    Update generate_parenthesis.cpp

--- C++/generate_parenthesis.cpp
+++ C++/generate_parenthesis.cpp
@@ -1,3 +1,18 @@
+/*
+    Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+Accepted
+*/
+
 class Solution {
 public:
     struct ListNode {
