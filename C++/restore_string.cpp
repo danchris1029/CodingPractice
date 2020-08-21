1e8b3f7f9f722686de5656060bf948c5c41afa1d
Christian Guardiola <61804256+danchris1029@users.noreply.github.com>
2020-08-21 18:49:49 -0500

    Create restore_string.cpp

---
+++ C++/restore_string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string shuffledString = s;
+        for(int i = 0; i < s.length(); i++)
+            shuffledString[indices[i]] = s[i];
+        
+        return shuffledString;
+    }
+};
